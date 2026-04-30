441341397f77ab4c5cca0c3222225dc13533f915
krishakoliya6150 <krishakoliya0@gmail.com>
2026-04-30 19:10:58 +0530

    Updated backend CORS for Vercel

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,10 +4,21 @@ const cors = require("cors")
 
 const app = express()
 
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://interview-seven-mauve.vercel.app"
+]
+
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true)
+        } else {
+            callback(new Error("Not allowed by CORS"))
+        }
+    },
     credentials: true
 }))
 
@@ -15,11 +26,8 @@ app.use(cors({
 const authRouter = require("./routes/auth.routes")
 const interviewRouter = require("./routes/interview.routes")
 
-
 /* using all the routes here */
 app.use("/api/auth", authRouter)
 app.use("/api/interview", interviewRouter)
-
-
 
 module.exports = app
